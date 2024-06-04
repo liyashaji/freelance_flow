b1d77cefbfd9c74c4a7a6ba923ecf4390f917660
liya shaji <liyaelizabethshaji@gmail.com>
2024-06-04 09:13:16 +0530

    Data added

--- app.py
+++ app.py
@@ -1,11 +1,39 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Graphics Designer',
+        'salary': 'Rs.1000 pw'
+    },
+    {
+        'id': 2,
+        'title': 'UI/UX designer',
+        'salary': 'Rs.5000 pw'
+    },
+    {
+        'id': 3,
+        'title': 'Guitar Teacher',
+        'salary': 'Rs.7000'
+    },
+    {
+        'id': 4,
+        'title': 'Web Developer',
+        'salary': 'Rs.5000'
+    },
+    {
+        'id': 5,
+        'title': 'App Developer',
+        'salary': 'Rs.10000'
+    }
+]
 
+@app.route("/")
+def hello_freelance():
+    return render_template('home.html', jobs=JOBS)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug =True)
+    app.run(host='0.0.0.0', debug=True)
