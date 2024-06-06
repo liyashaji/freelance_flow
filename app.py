2fe3960baf188dedb9d756684eae7ebe6f25fec5
liya shaji <liyaelizabethshaji@gmail.com>
2024-06-06 19:23:54 +0530

    New route added

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import jsonify
 
 app = Flask(__name__)
 
@@ -34,6 +35,11 @@ JOBS = [
 @app.route("/")
 def hello_freelance():
     return render_template('home.html', jobs=JOBS)
+
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify (JOBS)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
