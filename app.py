037714ba67d8301ea347d36632ee19577fb72910
liya shaji <liyaelizabethshaji@gmail.com>
2024-06-09 12:46:42 +0530

    Database integrated

--- app.py
+++ app.py
@@ -1,40 +1,36 @@
 from flask import Flask
 from flask import render_template
 from flask import jsonify
-
+from database import engine
+from sqlalchemy import text
 app = Flask(__name__)
 
-JOBS = [
-    {
-        'id': 1,
-        'title': 'Graphics Designer',
-        'salary': 'Rs.1000 pw'
-    },
-    {
-        'id': 2,
-        'title': 'UI/UX designer',
-        'salary': 'Rs.5000 pw'
-    },
-    {
-        'id': 3,
-        'title': 'Guitar Teacher',
-        'salary': 'Rs.7000'
-    },
-    {
-        'id': 4,
-        'title': 'Web Developer',
-        'salary': 'Rs.5000'
-    },
-    {
-        'id': 5,
-        'title': 'App Developer',
-        'salary': 'Rs.10000'
-    }
-]
+
+def load_jobs_from_db():
+     try:
+        with engine.connect() as conn:
+            # Create a text object for the SQL query
+            query = text("select * from jobs")
+            
+            # Execute the query
+            result = conn.execute(query)
+            
+            # Get the column names
+            column_names = [desc[0] for desc in result.cursor.description]
+            
+            # Convert the result rows to dictionaries
+            jobs = [dict(zip(column_names, row)) for row in result]
+            
+            return jobs
+     except Exception as e:
+        print(f"An error occurred while connecting to the database: {e}")
+
+
 
 @app.route("/")
 def hello_freelance():
-    return render_template('home.html', jobs=JOBS)
+    jobs = load_jobs_from_db()
+    return render_template('home.html', jobs=jobs)
 
 @app.route("/api/jobs")
 def list_jobs():
---
+++ database.py
@@ -0,0 +1,34 @@
+from sqlalchemy import create_engine, text
+
+# Service URI provided by Aiven
+service_uri = "mysql://avnadmin:AVNS_4NuPSCuz4nj7XXr36m4@mysql-19bbe057-freelanceflow.i.aivencloud.com:17897/defaultdb"
+
+# Custom SSL parameters
+ssl_args = {
+    "ssl": {
+        "ca": r"F:\desktop\freelance_flow\certs\ca.pem",
+        "check_hostname": False
+    }
+}
+
+# Create engine using the service URI and SSL parameters
+engine = create_engine(service_uri, connect_args=ssl_args, pool_pre_ping=True)
+
+# Test connection
+try:
+    with engine.connect() as conn:
+        # Create a text object for the SQL query
+        query = text("select * from jobs")
+        
+        # Execute the query
+        result = conn.execute(query)
+        
+        # Get the column names
+        column_names = [desc[0] for desc in result.cursor.description]
+        
+        # Convert the result rows to dictionaries
+        result_dicts = [dict(zip(column_names, row)) for row in result]
+        
+        print(result_dicts)    
+except Exception as e:
+    print(f"An error occurred while connecting to the database: {e}")
